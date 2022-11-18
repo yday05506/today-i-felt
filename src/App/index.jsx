553d002af8c63a53c0ccba8906ec2239d588f27d
yday <yangdayeon2@gmail.com>
2022-11-18 22:09:51 +0900

    오늘 기분, 상태, 있었던 일들

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react'; // 리액트 문법을 쓸 때 React Import
+// eslint-disable-next-line import/no-named-as-default
+import { BrowserRouter } from 'react-router-dom';
 import FeelingList from '../FeelingList';
-import FeelingDetail from '../FeelingDetail';
+import CurrentContents from '../CurrentContents';
 import './style.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <FeelingList />
-        <FeelingDetail />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <FeelingList />
+          <CurrentContents />
+        </div>
+      </BrowserRouter>
     );
   }
 }
